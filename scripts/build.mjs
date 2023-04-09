9ef56940aafeeace848d32547b697afbb0f359c3
flippedround <734243792@qq.com>
2023-04-09 18:11:55 +0800

    chore: update .gitignore

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -3,22 +3,13 @@ import { createSpinner } from 'nanospinner'
 import { resolve } from 'path'
 
 const CWD = process.cwd()
-const PKG_CLI = resolve(CWD, './packages/varlet-cli')
-const PKG_VITE_PLUGINS = resolve(CWD, './packages/varlet-vite-plugins')
-const PKG_ICONS = resolve(CWD, './packages/varlet-icons')
 const PKG_UI = resolve(CWD, './packages/varlet-ui')
 const PKG_SHARED = resolve(CWD, './packages/varlet-shared')
 const PKG_USE = resolve(CWD, './packages/varlet-use')
 
-export const buildCli = () => execa('pnpm', ['build'], { cwd: PKG_CLI })
-
-export const buildVitePlugins = () => execa('pnpm', ['build'], { cwd: PKG_VITE_PLUGINS })
-
 export const buildShared = () => execa('pnpm', ['build'], { cwd: PKG_SHARED })
 
 export const buildUse = () => execa('pnpm', ['build'], { cwd: PKG_USE })
-
-export const buildIcons = () => execa('pnpm', ['build'], { cwd: PKG_ICONS })
 
 export const buildUI = () => execa('pnpm', ['compile'], { cwd: PKG_UI })
 
@@ -36,8 +27,5 @@ export async function runTask(taskName, task) {
 export async function runTaskQueue() {
   await runTask('shared', buildShared)
   await runTask('use', buildUse)
-  await runTask('vite plugins', buildVitePlugins)
-  await runTask('cli', buildCli)
-  await runTask('icons', buildIcons)
   await runTask('ui', buildUI)
 }
